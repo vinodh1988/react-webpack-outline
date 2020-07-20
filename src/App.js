5dbc4b8c51ef2637a0437ec0eb2896f74965f569
vinodh1988 <k.c.vinodh@hotmail.com>
2020-07-20 20:52:12 +0530

    first commit:

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.css';
+
+const App = ()=>{
+    return(
+        <div>
+        <h1>React App is Up and Running...!!!</h1>
+        <h3>Furnished...</h3>
+        </div>
+    )
+}
+
+export default App;
